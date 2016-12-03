df73d348dc95df7c129b431350f0fc235255f084
Jwing28 <jonathan.wing.lee@gmail.com>
2016-12-02 18:48:58 -0800

    [update] remove template and put html into component

--- app/search.js
+++ app/search.js
@@ -5,8 +5,31 @@ myApp.controller('myController', ['$scope', function($scope) {
 
 }]);
 
-
-myApp.component('abc', {
-  templateUrl:'/app/templates/search.template.html',
-  bindings: { name: '@'}
+myApp.component('searchHeader', {
+  template:
+  `
+    <div class="menu">
+      <div class="btn-group" role="group">
+        <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle btn btn-info" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </button>
+        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+          <a class="dropdown-item" href="#">Favorites List</a>
+          <a class="dropdown-item" href="#">Login/Logout</a>
+        </div>
+      </div>   
+        
+      <div class="searchBar container-fluid">        
+        <h2>MyBooks</h2>
+        <input />
+        <button class="btn btn-primary" type="submit">Find Books</button>
+      </div>
+      <div class="searchResults"></div>
+    </div>
+    {{ $ctrl.person }}
+  `,
+  bindings: { name: '@'},
+  controller:function() {
+    this.person = "jon";
+  }
 });
