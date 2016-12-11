53111c8d6c5a235ff8d0a0d2da5c89b032235123
Jwing28 <jonathan.wing.lee@gmail.com>
2016-12-10 23:14:37 -0800

    [add] add render favorite books

--- app/favorites.js
+++ app/favorites.js
@@ -1,34 +1,35 @@
 var favorites = angular.module('myFavorites',[]);
 var favoritesCtrl = function () {}
+
 var renderFavoritesController = function ($scope) {
+  $scope.bookList = []; 
+  // firebase loads asynchronously
+  setTimeout(function() {
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var bkObj = snapshot.val();
+      var bkArr = [];
+      for (var key in bkObj) {        
+        bkArr.push(bkObj[key]);
+      }
 
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      //var user = firebase.auth().currentUser;
-      //var userBookListRef = firebase.database().ref('users/' + user.uid);
+      $scope.$apply(function(){
+        $scope.bookList = bkArr;
+      })
+    });    
+  },1000);
 
-      $scope.bookList = "";
-      //booklist being watched and on change it will update with changed list
-      userBookListRef.on('value', function(snapshot) {    
-        $scope.bookList = snapshot.val();//snapshot is our updated list
-      });      
-    } else {
-      // No user is signed in.
-      console.error('no user is signed in');
-    }
-  });  
-
+  //remove a favorited book from the list should setoff above
   this.removeFavorite = function(bookTitle) {    
     var user = firebase.auth().currentUser;    
-    firebase.database().ref('users/' + user.uid).orderByChild('title').equalTo(bookTitle).remove()
+    firebase.database().ref('users/' + user.uid).orderByChild('title').equalTo(bookTitle).remove();
     console.log(bookTitle + ' removed!');
   }
 }
 
 var renderFavoritesComponent = {
   template:`
-    <a></a>
+    <a>My Favorites</a>
     <div ng-repeat="book in bookList track by $index">
       <div class="card" ng-style="{float:'left', width:'30%', margin: '5px', height:'35em', 'background-color':'#fcf1d4'}">
         <div ng-style="{height: '100%', width:'100%' }">
@@ -37,7 +38,7 @@ var renderFavoritesComponent = {
             <h4 class="card-title">Title: {{ book.title }}</h4>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text">Published: {{ book.published }}</p>
-            <a href="#" ng-click="$ctrl.removeFavorite(book.title)" class="btn btn-primary">Add to Favorites</a> 
+            <a href="#" ng-click="$ctrl.removeFavorite(book.title)" class="btn btn-primary">Remove Book</a> 
             <p>Count:{{ $index }}</p>
           </div>
         </div>
@@ -48,6 +49,11 @@ var renderFavoritesComponent = {
 }
 
 favorites.controller('myFavoritesCtrl',favoritesCtrl);
-favorites.component('renderFavorites',renderFavoritesComponent);
+favorites.component('test',renderFavoritesComponent);
+
+// favorites.component('test',{
+//   template:'<div>this works</div>',
+//   controller: function () {}
+// });
 
 
