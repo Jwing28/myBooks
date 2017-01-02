3245b8a3340d0b9f25237032f4c53a046e8367de
Jwing28 <jonathan.wing.lee@gmail.com>
2017-01-01 18:19:19 -0800

    [update] update to run app on localhost and replaced template with templateUrl within login component

--- app/login.js
+++ app/login.js
@@ -46,41 +46,8 @@ var login = function ($scope) {
 }
 
 var loginComponent = {
-  template: `
-    <h2 ng-style="{textAlign:'center', marginTop:'20px'}">New User or Existing User</h2>
-    <div class="bg-danger text-white" ng-bind="signup.error"></div>
-    <span>
-      <form action="./search.html" ng-style="{position:'absolute', top:'30%', left:'20%', 'background-color':'#fcf1d4', padding:'10px'}">
-        <h3>MyBooks: Sign-Up</h3>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input ng-model="$ctrl.emailSignup" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input ng-model="$ctrl.passwordSignup" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>      
-        <button ng-href="{{ ($ctrl.passwordSignup.length > 5) && $ctrl.emailSignup ? "./search.html" : "#" }}" type="submit" class="btn btn-primary" ng-click="$ctrl.signUp()">Sign-Up</button>        
-      </form>      
-    </span>
-    <span>
-      <div class="bg-danger text-white" ng-bind="login.error"></div>
-      <form action="./search.html" ng-style="{position:'absolute', top:'30%', left:'60%', 'background-color':'#fcf1d4', padding:'10px'}">
-        <h3>MyBooks: Login</h3>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input ng-model="$ctrl.emailLogin" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input ng-model="$ctrl.passwordLogin" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>      
-        <button ng-href="{{ ($ctr.passwordLogin.length > 5) && $ctrl.emailLogin ? './search.html' : '#' }}" type="submit" class="btn btn-primary" ng-click="$ctrl.login()">Login</button>                
-      </form>      
-    </span>    
-  `,
+  templateUrl:'./templates/login.template.html'
+,
   controller: ['$scope', login]
 }
 
