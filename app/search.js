bfe053f01802a8a81dec81bd34da11fc2e8a3baa
Jwing28 <jonathan.wing.lee@gmail.com>
2017-01-16 15:18:20 -0800

    [update] separate component templates into separate files

--- app/search.js
+++ app/search.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp',[]);
+rvar myApp = angular.module('myApp',[]);
 var myController = function () {}
 var database = firebase.database();// reference to database
 console.log(database);
@@ -71,54 +71,15 @@ var renderBooksController = function ($rootScope, $scope) {
  
  //at <a></a> add to favorites - this is where you'll have to somehow add the given book  
 var renderBooksComponent = {
-  template:`
-    <div class="card-deck-wrapper">
-      <div class="card-deck">
-        <div ng-repeat="book in list track by $index">  
-          <div class="card" ng-style="{float:'left', width:'30%', margin: '5px', height:'35em', 'background-color':'#fcf1d4'}">
-            <div ng-style="{height: '100%', width:'100%' }">
-              <img class="card-img-top" ng-src="{{ book.volumeInfo.imageLinks.smallThumbnail }}" alt="Card image cap">
-              <div class="card-block">
-                <h4 class="card-title">Title: {{ book.volumeInfo.title }}</h4>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text">Published: {{ book.volumeInfo.publishedDate }}</p>
-                <a href="#" ng-click="$ctrl.addToFavor($index)" class="btn btn-primary">Add to Favorites</a> 
-                <p>Count:{{ $index }}</p>
-              </div>
-            </div>
-          </div>
-        </div> 
-      </div>       
-    </div>   
-  `,
+  templateUrl:'./templates/search.renderBooks.template.html'
+  ,
   controller: ['$rootScope', '$scope', renderBooksController]
 }
 
 var searchHeaderComponent = {
   transclude: true,  
-  template:`
-    <div ng-transclude class="menu">
-      <div class="btn-group" role="group" ng-style="{display:'inline'}">
-        <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle btn btn-info" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </button>
-        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-          <a class="dropdown-item" href="./favorites.html">Favorites List</a>
-          <a class="dropdown-item" href="./login.html">Login/Logout</a>
-        </div>
-      </div>   
-
-      <div ng-bind="'Hi, ' + currentUser + '!'" ng-style="{float:right, marginLeft:'15px'}"></div>        
-        
-      <div class="container-fluid" ng-style="{width:'1100px', marginLeft:'20%'}">        
-        <h2>MyBooks</h2>
-        <input ng-model="$ctrl.search" />
-        <button ng-click="$ctrl.getBooks()" class="btn btn-primary" type="submit">Find Books</button>
-        <hr>
-      <render-Books></render-Books>
-      </div>    
-    </div>
-  `,
+  templateUrl:'./templates/search.searchHeader.template.html'
+  ,
   bindings: { 
     search: '@', //user's input in input box
   },
